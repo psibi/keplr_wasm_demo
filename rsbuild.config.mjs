2ca029ed4f08978c509bb34f1ca2957de944300c
Sibi Prabakaran <sibi@psibi.in>
2024-11-05 11:44:54 +0530

    format

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,22 +1,21 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 export default {
-  source: {
-    	  entry: {
-	  	 hello: './index.js'
-	  }
-  },
-  tools: {
-  	 rspack: {
-	 	 plugins: [
-		 	 new HtmlWebpackPlugin(),
-        new WasmPackPlugin({
-            crateDirectory: path.resolve(__dirname, ".")
-        }),
-
-		 		 ]
-	 }
-  }
+	source: {
+		entry: {
+			hello: "./index.js",
+		},
+	},
+	tools: {
+		rspack: {
+			plugins: [
+				new HtmlWebpackPlugin(),
+				new WasmPackPlugin({
+					crateDirectory: path.resolve(__dirname, "."),
+				}),
+			],
+		},
+	},
 };
